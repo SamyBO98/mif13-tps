47ad8194b8f0dda5cb7fafd4e165caa6ba76b702
Azziz <otman.azziz@etu.univ-lyon1.fr>
2021-03-11 18:25:48 +0100

    deleting tp1 files to got only tp3's files

--- users/src/main/java/fr/univlyon1/mif13/tp1/exception/MissingArgumentsException.java
+++
@@ -1,9 +0,0 @@
-package fr.univlyon1.mif13.tp1.exception;
-
-public class MissingArgumentsException extends RuntimeException {
-
-    public MissingArgumentsException(){
-        super("Missing arguments");
-    }
-
-}
