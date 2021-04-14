981c93ad6285d13cf92e946363d4f2e9ac612042
Azziz <otman.azziz@etu.univ-lyon1.fr>
2021-04-14 10:16:40 +0200

    test

--- users/src/main/java/fr/univlyon1/mif13/tp1/controller/OperationController.java
+++ users/src/main/java/fr/univlyon1/mif13/tp1/controller/OperationController.java
@@ -45,7 +45,7 @@ public class OperationController {
             @ApiResponse(responseCode = "404", description = "Wrong login or password")
     })
     @PostMapping("/login")
-    @CrossOrigin(origins = {"http://localhost", "http://192.168.75.118", "https://192.168.75.118"})
+    @CrossOrigin(origins = {"http://localhost:*", "http://192.168.75.118", "https://192.168.75.118"})
     public ResponseEntity<Void> login(@RequestParam("login") @Schema(example = "otman-le-rigolo") String login, @RequestParam("password") @Schema(example = "password") String password, @RequestHeader("Origin") String origin) {
         //Check if the user exists
         Optional<User> opUser = userDao.get(login);
@@ -79,7 +79,7 @@ public class OperationController {
             @ApiResponse(responseCode = "404", description = "Error: User not exists / Token is wrong / User is not connected")
     })
     @DeleteMapping("/logout")
-    @CrossOrigin(origins = {"http://localhost", "http://192.168.75.118", "https://192.168.75.118"})
+    @CrossOrigin(origins = {"http://localhost:*", "http://192.168.75.118", "https://192.168.75.118"})
     public ResponseEntity<Void> logout(@RequestHeader("Authorization") String token){
         //Get the request servlet
         HttpServletRequest request = getRequest();
@@ -111,7 +111,7 @@ public class OperationController {
             @ApiResponse(responseCode = "404", description = "Error: Token is wrong / User is not connected")
     })
     @GetMapping("/authenticate")
-    @CrossOrigin(origins = {"http://localhost", "http://192.168.75.118", "https://192.168.75.118"})
+    @CrossOrigin(origins = {"http://localhost:*", "http://192.168.75.118", "https://192.168.75.118"})
     public ResponseEntity<Void> authenticate(@RequestParam("token") @Schema(example = "edit-this-token") String token, @RequestParam("origin") @Schema(example = "*/*") String origin) {
         //Get the request servlet
         HttpServletRequest request = getRequest();
