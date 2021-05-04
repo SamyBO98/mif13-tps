da90996df2bd82f93b7d4a9c4db85790cf5bd710
Azziz <otman.azziz@etu.univ-lyon1.fr>
2021-05-04 16:25:02 +0200

    test geolocalisation on vm

---
+++ test-geolocalisation/map.js
@@ -0,0 +1,45 @@
+// initialisation de la map
+let zoom = 10;
+let myPosition = null;
+
+let mymap = L.map('map', {
+    center: [45.782, 4.8656],
+    zoom: zoom
+});
+
+// Création d'un "tile layer" (permet l'affichage sur la carte)
+L.tileLayer('https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.jpg90?access_token=pk.eyJ1IjoibTFpZjEzIiwiYSI6ImNqczBubmhyajFnMnY0YWx4c2FwMmRtbm4ifQ.O6W7HeTW3UvOVgjCiPrdsA', {
+		maxZoom: 22,
+		minZoom: 1,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken: 'pk.eyJ1IjoibTFpZjEzIiwiYSI6ImNqczBubmhyajFnMnY0YWx4c2FwMmRtbm4ifQ.O6W7HeTW3UvOVgjCiPrdsA'
+	}).addTo(mymap);
+
+function showMap(position) {
+	// Ajout d'un marker
+	if (myPosition !== null) myPosition.remove(mymap);
+	let lat = position.coords.latitude;
+	let lng = position.coords.longitude;
+
+	myPosition = L.marker([lat, lng]).addTo(mymap).bindPopup('<strong>Votre position</strong>').openPopup();
+
+	// Affichage à la nouvelle position
+	mymap.setView([lat, lng], zoom);
+
+	return false;
+}
+
+// Mise à jour de la map
+function updateMap() {
+	navigator.geolocation.getCurrentPosition(showMap);
+
+	// La fonction de validation du formulaire renvoie false pour bloquer le rechargement de la page.
+	return false;
+}
+
+setInterval(updateMap, 5000);
